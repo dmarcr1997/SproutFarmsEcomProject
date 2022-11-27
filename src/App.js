027cdb3b5f5d6b6e3d80c194f8c249e9ce934238
Damon Marc Rocha II <dmarcr1997@gmail.com>
2022-11-26 23:41:11 -0500

    Nov-26#1: Basic App Layout

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Microgreens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Mushrooms</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Subscriptions</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Recipies</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Learn</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

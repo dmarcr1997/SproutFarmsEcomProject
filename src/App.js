3bef9229940b8f93622ce59bfe146ca63c177a50
Damon Marc Rocha II <dmarcr1997@gmail.com>
2022-12-01 12:56:18 -0500

    Add routes and base navigation component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-import Directory from './components/directory/directory.component';
+const Shop = () => {
+  return <h1>I am the Shop</h1>
+}
 const App = () => {
 
-  return (
-    <Directory />
+  return (  
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}/>
+        <Route path="shop" element={<Shop />}/>
+      </Route>
+    </Routes>
   );
 }
 
